34eae33f83da6d80628ac0adb898c08d658f6d7f
npb <noahpberman@gmail.com>
2020-01-06 07:05:13 -0500

    Starting eventlayer

--- code/pygamelayer/eventlayer.py
+++ code/pygamelayer/eventlayer.py
@@ -1,9 +1,33 @@
 from queue import Queue
 
+from pygame.locals import *
+
+from code.enums.events import Events
+from code.datashare import gamequit
+
 class Event:
     def __init__(self, event):
         # TODO: Make this interpret pygame event
-        self.ev = None
+        try:
+            self.ev = {
+                ACTIVEEVENT: Events.ACTIVEEVENT,
+                KEYDOWN: Events.KEYDOWN,
+                KEYUP: Events.KEYUP,
+                MOUSEMOTION: Events.MOUSEMOTION,
+                MOUSEBUTTONDOWN: Events.MOUSEBUTTONDOWN,
+                MOUSEBUTTONUP: Events.MOUSEBUTTONUP,
+                JOYAXISMOTION: Events.JOYAXISMOTION,
+                JOYBALLMOTION: Events.JOYBALLMOTION,
+                JOYHATMOTION: Events.JOYHATMOTION,
+                JOYBUTTONDOWN: Events.JOYBUTTONDOWN,
+                JOYBUTTONUP: Events.JOYBUTTONUP,
+                VIDEORESIZE: Events.VIDEORESIZE,
+                VIDEOEXPOSE: Events.VIDEOEXPOSE,
+                USEREVENT: Events.USEREVENT
+            }[event.type]
+        except:
+            print(f'ERROR: EVENT OF TYPE {event.type} NOT RECOGNIZED')
+            gamequit.game_quit = True
         self.args = None
 
 class EventLayer:
