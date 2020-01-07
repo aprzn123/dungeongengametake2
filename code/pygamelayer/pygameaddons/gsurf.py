022cfe98f784047a27c15ef935960362dfa6ca23
npb <noahpberman@gmail.com>
2020-01-07 17:06:43 -0500

    working on planning and key mapping

--- code/pygamelayer/pygameaddons/gsurf.py
+++ code/pygamelayer/pygameaddons/gsurf.py
@@ -2,5 +2,6 @@ import pygame
 class GSurface(pygame.Surface):
     def get_surface(self):
         return self
+    
     def blit(self, source, dest, area=None, special_flags=0):
         super().blit(source.get_surface, dest, area, special_flags)
---
+++ code/utils/revdict.py
@@ -0,0 +1,5 @@
+class ReversibleDict():
+    def __init__(self, i_dict):
+        self.contained_dict = i_dict
+    
+    
